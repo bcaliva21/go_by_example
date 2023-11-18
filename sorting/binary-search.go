9e2f5cdb2e361547fbf588e7d147f153b6c4405c
Bradley Caliva <bcaliva21@gmail.com>
2023-11-18 11:26:03 -0600

    feat(sorting): create sorting dir; implement binary-search algorithm;

---
+++ sorting/binary-search.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func binarySort (a []int, t int) int {
+	s := 0
+	e := len(a)-1
+	m := (s+e)/2
+
+	for s < e {
+		if a[m] > t {
+			e = m-1
+			m = (s+e)/2
+		} else {
+			s = m+1
+			m = (s+e)/2
+		}
+	}
+
+	if a[m] == t {
+		return m
+	} else {
+		return -1
+	}
+}
+
+func main() {
+	a := []int{1,3,5,7,9,11,13,15,17,19,21,23,25,27,29}
+
+	fmt.Println(binarySort(a, 1))
+	fmt.Println(binarySort(a, 29))
+	fmt.Println(binarySort(a, 0))
+	fmt.Println(binarySort(a, 100))
+}
+
