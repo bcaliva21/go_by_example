4fb84b5a1cdd2a38e2fe66208862c31e16968ced
Bradley Caliva <bcaliva21@gmail.com>
2023-11-19 15:33:59 -0600

    feat(sorting/selection): correct file name to match algorithm

---
+++ sorting/selection.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Selection (a []int) []int {
+	for i := 0; i < len(a); i++ {
+		m := a[i]
+		mi := i
+		for j := i; j < len(a); j++ {
+			if a[j] < m {
+				m = a[j]
+			    mi = j
+			}
+		}
+		a[i], a[mi] = a[mi], a[i]
+	}
+
+	return a
+}
+
+func main() {
+	arr := []int{10,8,78,64,20,0}
+
+	fmt.Println(arr)
+	
+	fmt.Println(Selection(arr))
+
+}
