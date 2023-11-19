ad385e69d44bdcafcdfc9f19116d2af3cbecae2b
Bradley Caliva <bcaliva21@gmail.com>
2023-11-19 15:55:23 -0600

    feat(sorting/insertion): implement insertion sort

--- sorting/insertion.go
+++ sorting/insertion.go
@@ -1,3 +1,27 @@
 package main
 
-func insertion
+import "fmt"
+
+func Insertion (a []int) []int {
+
+	for i := 1; i < len(a); i++ {
+		val, j := a[i],i
+
+		for j > 0 && a[j-1] > val {
+			a[j] = a[j -1]
+			j--
+		}
+		a[j] = val
+	}
+
+	return a
+}
+
+func main () {
+	test := []int{10,8,6,4,2,0}
+
+	fmt.Println(test)
+	fmt.Println(Insertion(test))
+
+}
+
