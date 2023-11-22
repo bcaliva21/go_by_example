4eca4447f8741feccb210ea7da3da619e9d10a8e
Bradley Caliva <bcaliva21@gmail.com>
2023-11-22 14:48:46 -0600

    feat(sorting/heap): implement HeapSort;

---
+++ sorting/heap.go
@@ -0,0 +1,125 @@
+package main
+
+import "fmt"
+
+type MaxHeap struct {
+	List []int
+}
+
+func (h *MaxHeap) Insert(n int) {
+	h.List = append(h.List, n)
+	h.HeapifyUp()
+}
+
+func (h *MaxHeap) Extract() int {
+	l := len(h.List) - 1
+	o := h.List[0]
+
+	if l < 1 {
+		return -1
+	}
+
+	h.List[0] = h.List[l]
+	h.List = h.List[:l]
+
+	h.HeapifyDown()
+
+	return o
+}
+
+func (h *MaxHeap) HeapifyDown() {
+	i := 0
+	last := len(h.List) - 1
+	c := 0
+	l := h.GetLeft(i)
+	r := h.GetRight(i)
+
+	for l <= last {
+		if l == last {
+			c = l
+		} else if h.List[l] > h.List[r] {
+			c = l
+		} else {
+			c = r
+		}
+
+		if h.List[i] < h.List[c] {
+			h.Swap(i,c)
+			i = c
+			l,r = h.GetLeft(i), h.GetRight(i)
+		} else {
+			return
+		}
+	}
+}
+
+func (h *MaxHeap) SiftDown(last int) {
+	c,i := 0,0
+	l, r := h.GetLeft(i), h.GetRight(i)
+
+	for l <= last {
+		if l == last {
+			c = l
+		} else if h.List[l] > h.List[r] {
+			c = l
+		} else {
+			c = r
+		}
+
+		if h.List[i] < h.List[c] {
+			h.Swap(i,c)
+			i = c
+			l,r = h.GetLeft(i), h.GetRight(i)
+		} else {
+			return
+		}
+	}
+}
+
+func (h *MaxHeap) HeapifyUp() {
+	i := len(h.List) - 1
+	p := h.GetParent(i)
+
+	for p > -1 && h.List[i] > h.List[p] {
+		h.Swap(i,p)
+		i = p
+		p = h.GetParent(i)
+	}
+}
+
+func (h *MaxHeap) GetParent(i int) int {
+	return (i-1)/2 
+}
+
+func (h *MaxHeap) GetLeft(i int) int {
+	return (i*2)+1
+}
+
+func (h *MaxHeap) GetRight(i int) int {
+	return (i*2)+2
+}
+
+func (h *MaxHeap) Swap(i1, i2 int) {
+	h.List[i1], h.List[i2] = h.List[i2], h.List[i1]
+}
+
+func HeapSort (a []int) []int {
+	h := &MaxHeap{}
+	for _,v := range a {
+		h.Insert(v)
+	}
+
+	for i := len(h.List) - 1; i > -1; i-- {
+		h.Swap(0,i)
+		h.SiftDown(i-1)
+	}
+
+	return h.List
+}
+
+func main() {
+	a := []int{10,39,8,6,34,4,21,11,0,-10,94,0,-4,99}
+
+	fmt.Println(a)
+	fmt.Println(HeapSort(a))
+}
